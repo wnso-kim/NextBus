3a13195ab41e1a8f96ad12f968700e1dc4e4bbe0
wnso-kim <wnso.kim@gmail.com>
2023-10-28 19:23:40 +0900

    add: spring boot 프로젝트 추가

---
+++ src/main/java/com/wnso/NextBus/NextBusApplication.java
@@ -0,0 +1,13 @@
+package com.wnso.NextBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wnso/NextBus/NextBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wnso.NextBus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
